d8288436b977c29f407898fc8222ae1306377872
beno1234 <benolopesdias@gmail.com>
2025-04-16 01:27:41 -0300

    cors

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -2,7 +2,6 @@ import axios from "axios";
 import { NextResponse } from "next/server";
 import nodemailer from "nodemailer";
 
-// Create and configure Nodemailer transporter
 const transporter = nodemailer.createTransport({
   service: "gmail",
   host: "smtp.gmail.com",
